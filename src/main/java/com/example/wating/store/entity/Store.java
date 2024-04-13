2d7789ee0f51d8b894a544a14ef8ca93f5252a49
0empty <117061586+zeroempty2@users.noreply.github.com>
2024-04-13 21:12:45 +0900

    store 컬럼 추가

--- src/main/java/com/example/wating/store/entity/Store.java
+++ src/main/java/com/example/wating/store/entity/Store.java
@@ -1,11 +1,14 @@
 package com.example.wating.store.entity;
 
+import com.example.wating.user.entity.User;
 import com.example.wating.util.TimeStamped;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import java.time.LocalTime;
 import lombok.Builder;
 import lombok.Getter;
@@ -21,21 +24,44 @@ public class Store extends TimeStamped {
   private Long id;
 
   @Column
-  private Long ownerId;
-
-  @Column
   private String storeName; //가맹점이름
 
   @Column
-  private Float starRate = 0F; //별점
+  private String storeLocation; //가맹점위치정보
+
+  @Column
+  private Float starRate; //별점
+
+  @Column
+  private String timeDetail; // 변수명 변경 예정, 운영시간
+
+  @Column
+  private String storePhoneNumber;  //가게전화번호
 
   @Column
   private String aboutStore;  //가게소개
 
+  @Column
+  private String storeNotification; //가맹점공지
+
+  @Column
+  private String reservationTypeFlag; //예약 지정일자 오픈 여부(Y,N)
+
+  @Column
+  private String regularHoliday; //정기 휴무일
+
+  @Column
+  private String storeHomepage; //홈페이지 주소
+
   @Builder
-  public Store(String storeName,String aboutStore,Long ownerId) {
+  public Store(String storeName,String aboutStore,User user) {
     this.storeName = storeName;
     this.aboutStore = aboutStore;
-    this.ownerId = ownerId;
+    this.user = user;
   }
+
+  @ManyToOne
+  @JoinColumn(name="user_id")
+  private User user;
+
 }
